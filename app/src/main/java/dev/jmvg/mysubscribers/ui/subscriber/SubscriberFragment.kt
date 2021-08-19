66fa0729d143058b5577e5e6d2557c2a174942e1
Joanantha Matheus <jmvg.comp@gmail.com>
2021-08-19 11:48:58 -0300

    Tela inicial da tela de cadastro de inscrito

---
+++ app/src/main/java/dev/jmvg/mysubscribers/ui/subscriber/SubscriberFragment.kt
@@ -0,0 +1,22 @@
+package dev.jmvg.mysubscribers.ui.subscriber
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import dev.jmvg.mysubscribers.R
+
+class SubscriberFragment : Fragment() {
+
+
+  private lateinit var viewModel: SubscriberViewModel
+
+  override fun onCreateView(
+    inflater: LayoutInflater, container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View? {
+    return inflater.inflate(R.layout.subscriber_fragment, container, false)
+  }
+
+}
